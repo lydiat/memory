0d3ac2de0e871957fb1f9b9adb7c609a2bfadc3e
Lydia Wagner <lydiawagner@gmail.com>
2016-07-25 09:47:01 -0400

    Change to PHP

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <title>Memory</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.5/TweenLite.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.5/plugins/CSSPlugin.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.4/easing/EasePack.min.js"></script>
+    <script src="js/memory.js"></script>
+</head>
+
+<body>
+    <div id="container"></div>
+    <div id="cardShell">
+        <div class="cardWrapper">
+            <div class="card">
+                <div class="cardFace front"></div>
+                <div class="cardFace back"><img /></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
